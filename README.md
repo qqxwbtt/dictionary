b8da61894316554cd6512690953f555c23a90db7
xuwenbo <qqxwbtt1@163.com>
2018-08-02 10:12:24 +0800

    cc

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 安装 npm install -g xwb_dictionary;
 爬取有道字典的数据实现命令行字典功能;
 无聊写的玩
+登入npm账户  npm login;
+npm publish 推送到npm后在输入安装命令
 ~~~
--- index.js
+++ index.js
@@ -3,20 +3,28 @@
 const cheerio = require('cheerio')
 const request = require("request");
 const http = require("http");
+const https = require("https");
+const iconv = require('iconv-lite');
+const isCh = require("is-chinese");
 const fs = require("fs");
+const Spinner = require('cli-spinner').Spinner;
 
 const word = process.argv.slice(2).join(" ");
-const url =  "http://www.hanhande.com/manhua/narutocn/1156594_2.shtml";
+const url =  encodeURI(`http://dict.youdao.com/w/eng/${word}/#keyfrom=dict2.index`);
+
+const  spinner = new Spinner('正在查询中.. %s');
+spinner.setSpinnerString('|/-\\');
+spinner.start();
 
 request(url, function(err, res, body) {
     // console.log(body);
+    spinner.stop(true);
     const $ = cheerio.load(body);
-    let result = $('div.mhBoxShow > div#pictureContent > p > img')[0].attribs.src
-    console.log(result);
-    // result = $(' div#phrsListTab > h2.wordbook-js > div.baav > p').text() + result;
-    // if(result == '') {
-    //     result = $('div#fanyiToggle > div.trans-container > p:nth-child(2)').text();
-    // }
-    // console.log(result.replace(/\s+/g, " "))
+    let result = $('#phrsListTab > .trans-container > ul').text()
+    result = $(' div#phrsListTab > h2.wordbook-js > div.baav > span').text() + result;
+    if(result == '') {
+        result = $('div#fanyiToggle > div.trans-container > p:nth-child(2)').text();
+    }
+    console.log(result.replace(/\s+/g, " "))
 })
 
